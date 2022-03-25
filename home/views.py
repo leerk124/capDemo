cf4b180afa4cba0869270d9042309b07b35b5923
Robin Lee <leerk124@gmail.com>
2022-03-25 15:05:25 -0400

    Added remaining coin pages

--- home/views.py
+++ home/views.py
@@ -7,8 +7,36 @@ import PlotlyTest
 def home(requests):
     return render(requests, 'home/welcome.html')
 
-
 def bitcoin(requests):
     divTEST = PlotlyTest.main('bitcoin')
     context = {'graph': divTEST}
     return render(requests, 'home/bitcoin.html', context)
+
+def cardano(requests):
+    return render(requests, 'home/cardano.html')
+
+def doge(requests):
+    return render(requests, 'home/doge.html')
+
+def ethereum(requests):
+    return render(requests, 'home/ethereum.html')
+
+
+def helium(requests):
+    return render(requests, 'home/helium.html')
+
+
+def litecoin(requests):
+    return render(requests, 'home/litecoin.html')
+
+
+def polkadot(requests):
+    return render(requests, 'home/polkadot.html')
+
+
+def shibainu(requests):
+    return render(requests, 'home/shibainu.html')
+
+
+def tether(requests):
+    return render(requests, 'home/tether.html')
