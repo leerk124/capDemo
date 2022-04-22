1c5f51f9f5e0128f50005851d0ae03a7a2d9e670
Robin Lee <leerk124@gmail.com>
2022-04-22 17:31:02 -0400

    Updated Homepage

--- static/js/sb-admin-2.js
+++ static/js/sb-admin-2.js
@@ -53,4 +53,21 @@
     e.preventDefault();
   });
 
+  //dropdown submenu
+  $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+  }
+  var $subMenu = $(this).next(".dropdown-menu");
+  $subMenu.toggleClass('show');
+
+
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass("show");
+  });
+
+
+  return false;
+});
+
 })(jQuery); // End of use strict
